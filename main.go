67b2020bb0726056ad6d84b3c3a9f792ee519f71
greycodee <zhengminghui99@gmail.com>
2022-06-15 11:49:28 +0800

    添加静态页面

--- main.go
+++ main.go
@@ -9,26 +9,49 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "/mnt/c/Users/zheng/Desktop/wxfileindex_plaintext.db")
+	db, err := sql.Open("sqlite3", "/Users/zheng/Documents/wcdb/wxfileindex_plaintext.db")
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer db.Close()
 
-	rows, err := db.Query("select username from WxFileIndex2 where msgId=9127")
+	db2, err := sql.Open("sqlite3", "/Users/zheng/Documents/wcdb/enmicromsg_plaintext.db")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db2.Close()
+	rows, err := db2.Query("select content from message where msgId=9127")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 	for rows.Next() {
+		var content string
+		err = rows.Scan(&content)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(content)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows2, err := db.Query("select username from WxFileIndex2 where msgId=9127")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows2.Close()
+	for rows2.Next() {
 		var username string
-		err = rows.Scan(&username)
+		err = rows2.Scan(&username)
 		if err != nil {
 			log.Fatal(err)
 		}
 		fmt.Println(username)
 	}
-	err = rows.Err()
+	err = rows2.Err()
 	if err != nil {
 		log.Fatal(err)
 	}
