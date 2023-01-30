227ddc43f1e3560677003b680dc31d261626e8e4
greycode <zhengminghui99@gmail.com>
2023-01-30 21:52:52 +0800

    start refactor

--- main.go
+++ main.go
@@ -1,24 +1,18 @@
 package main
 
 import (
-	"embed"
-	"encoding/json"
 	"flag"
-	"io/fs"
+	"fmt"
 	"log"
-	"net"
 	"net/http"
-	"strconv"
+	"time"
 
-	"github.com/greycodee/wechat-backup/db"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/sync/errgroup"
 )
 
-var wcdb *db.WCDB
-
-//go:embed static
-var htmlFile embed.FS
-
-var serverPort = flag.String("p", "9999", "server port")
+var apiPort = flag.String("ap", "9999", "api port")
+var htmlPort = flag.String("hp", "9991", "html port")
 var basePath = flag.String("f", "", "wechat bak folder")
 
 func init() {
@@ -28,112 +22,57 @@ func init() {
 	}
 }
 
+var (
+	g errgroup.Group
+)
+
+func htmlRouter() http.Handler {
+	e := gin.New()
+	e.Use(gin.Recovery())
+	e.StaticFS("/", http.Dir("./static"))
+	return e
+}
+
+func apiRouter() http.Handler {
+	e := gin.New()
+	e.Use(gin.Recovery())
+	e.GET("/", func(c *gin.Context) {
+		c.JSON(
+			http.StatusOK,
+			gin.H{
+				"code":    http.StatusOK,
+				"message": "Welcome server 02",
+			},
+		)
+	})
+
+	return e
+}
+
 func main() {
-
-	wcdb = db.InitWCDB(*basePath)
-
-	fsys, _ := fs.Sub(htmlFile, "static")
-	staticHandle := http.FileServer(http.FS(fsys))
-
-	// 文件路由
-	fs := http.FileServer(http.Dir(*basePath))
-	http.Handle(db.MediaPathPrefix, http.StripPrefix(db.MediaPathPrefix, fs))
-
-	http.Handle("/", staticHandle)
-	http.Handle("/api/", route())
-
-	log.Println("server start")
-	interfaceAddr, _ := net.InterfaceAddrs()
-	for _, address := range interfaceAddr {
-		ipNet, _ := address.(*net.IPNet)
-		if ipNet.IP.To4() != nil {
-			log.Printf("server addr http://%s:%s", ipNet.IP.String(), *serverPort)
-		}
+	htmlRouter := &http.Server{
+		Addr:         fmt.Sprintf(":%s", *htmlPort),
+		Handler:      htmlRouter(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
-	err := http.ListenAndServe(":"+*serverPort, nil)
-	if err != nil {
-		log.Fatalf("ListenAndServe: %v", err)
+	apiRouter := &http.Server{
+		Addr:         fmt.Sprintf(":%s", *apiPort),
+		Handler:      apiRouter(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
-}
 
-func route() http.Handler {
-	return &API{}
-}
+	g.Go(func() error {
+		return htmlRouter.ListenAndServe()
+	})
 
-type API struct {
-}
+	g.Go(func() error {
+		return apiRouter.ListenAndServe()
+	})
 
-func (api *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	apiMap[path](w, r)
-}
-
-var apiMap = map[string]func(w http.ResponseWriter, r *http.Request){
-	"/api/chat/list": func(w http.ResponseWriter, r *http.Request) {
-		// 聊天列表
-		params := r.URL.Query()
-		pageIndex, _ := strconv.Atoi(params["pageIndex"][0])
-		pageSize, _ := strconv.Atoi(params["pageSize"][0])
-		names, _ := params["name"]
-		name := ""
-		if len(names) > 0 {
-			name = names[0]
-		}
-		all, _ := strconv.ParseBool(params["all"][0])
-		result, err := json.Marshal(wcdb.ChatList(pageIndex-1, pageSize, all, name))
-		if err != nil {
-			log.Fatalf("json marshal error: %v", err)
-		}
-		w.Write(result)
-	},
-	"/api/chat/detail": func(w http.ResponseWriter, r *http.Request) {
-		//聊天记录
-		params := r.URL.Query()
-		talker := params["talker"][0]
-		pageIndex, _ := strconv.Atoi(params["pageIndex"][0])
-		pageSize, _ := strconv.Atoi(params["pageSize"][0])
-
-		result, err := json.Marshal(wcdb.ChatDetailList(talker, pageIndex-1, pageSize))
-		if err != nil {
-			log.Fatalf("json marshal error: %v", err)
-		}
-		w.Write(result)
-	},
-	"/api/user/info": func(w http.ResponseWriter, r *http.Request) {
-		// 用户信息
-		params := r.URL.Query()
-		username := params["username"][0]
-		result, err := json.Marshal(wcdb.GetUserInfo(username))
-		if err != nil {
-			log.Fatalf("json marshal error: %v", err)
-		}
-		w.Write(result)
-	},
-	"/api/user/myinfo": func(w http.ResponseWriter, r *http.Request) {
-		// 自己的信息
-		result, err := json.Marshal(wcdb.GetMyInfo())
-		if err != nil {
-			log.Fatalf("json marshal error: %v", err)
-		}
-		w.Write(result)
-	},
-	"/api/media/img": func(w http.ResponseWriter, r *http.Request) {
-		// 图片
-		params := r.URL.Query()
-		msgId := params["msgId"][0]
-		w.Write([]byte(wcdb.GetImgPath(msgId)))
-	},
-	"/api/media/video": func(w http.ResponseWriter, r *http.Request) {
-		// 视频
-		params := r.URL.Query()
-		msgId := params["msgId"][0]
-		w.Write([]byte(wcdb.GetVideoPath(msgId)))
-	},
-	"/api/media/voice": func(w http.ResponseWriter, r *http.Request) {
-		// 语音
-		params := r.URL.Query()
-		msgId := params["msgId"][0]
-		w.Write([]byte(wcdb.GetVoicePath(msgId)))
-	},
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
+	}
 }
